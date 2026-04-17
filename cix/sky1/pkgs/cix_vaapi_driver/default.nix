c9c391cc97c6c1529b9c62abd5a9c9f63751c59e
ZHANG Yuntian <yt@radxa.com>
2026-04-17 16:57:28 +0800

    fix: allow vaapi to build
    Signed-off-by: ZHANG Yuntian <yt@radxa.com>

--- cix/sky1/pkgs/cix_vaapi_driver/default.nix
+++ cix/sky1/pkgs/cix_vaapi_driver/default.nix
@@ -5,8 +5,16 @@
   cmake,
   pkg-config,
   cix_libva,
+  libdrm,
 }:
-
+let
+  vpu_driver = fetchFromGitHub {
+    owner = "cixtech";
+    repo = "cix_opensource__vpu_driver";
+    rev = "8010c94da3534398555bb53f48332981e1469149";
+    hash = "sha256-RRZPRHJBzwqw6vDL4TrPEoTcnQhFkOoh4JsDo6ky3e4=";
+  };
+in
 stdenv.mkDerivation (finalAttrs: {
   pname = "cix_vaapi_driver";
   version = "2026.Q1";
@@ -25,7 +33,21 @@ stdenv.mkDerivation (finalAttrs: {
 
   buildInputs = [
     cix_libva
+    libdrm
   ];
+
+  prePatch = ''
+    cp ${vpu_driver}/driver/linux/mvx-v4l2-controls.h devices/v4l2/
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/lib/dri
+    cp libcix_va_drv_video.so $out/lib/dri/
+
+    runHook postInstall
+  '';
 
   meta = {
     homepage = "https://github.com/cixtech/cix_vaapi";
